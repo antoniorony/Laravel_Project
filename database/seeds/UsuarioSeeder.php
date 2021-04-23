6e854d8ba3fd86bf4cfcebee808d75d5f1b1abe6
antoniorony <antoniorony2013@gmail.com>
2021-04-23 08:23:08 -0300

    Add pastas bootstrap, config, database

---
+++ database/seeds/UsuarioSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsuarioSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $dados = [
+            'name'=>"Antonio2",
+            'email'=>"admin@email.com",
+            'password'=>bcrypt("123456"),
+        ];
+
+        if (User::where('email', '=', $dados['email'])->count()) {
+            $usuario = User::where('email', '=', $dados['email'])->first();
+            $usuario->update($dados);
+
+            echo 'Atualizado';
+
+        } else {
+            User::create($dados);
+
+            echo 'Cadastrado';
+        }
+    }
+}
